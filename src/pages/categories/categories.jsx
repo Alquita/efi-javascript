e6f9710c0cbc353532da3581b612d303e55f938c
Joaquin-CasVar <j.castellanovarela@itecriocuarto.org.ar>
2025-11-03 12:03:15 -0300

    mostrar acciones dependiendo de el rol del usuario

--- src/pages/categories/categories.jsx
+++ src/pages/categories/categories.jsx
@@ -1,12 +1,27 @@
-import { Fragment, useEffect, useState } from "react"
+import { Fragment, useContext, useEffect, useState } from "react"
 import { Card } from 'primereact/card'
 import { Button } from "primereact/button"
 import { DataTable } from "primereact/datatable"
 import { Column } from 'primereact/column'
 import { ProgressSpinner } from 'primereact/progressspinner'
 import Navbar from "../../components/navbar"
+import { AuthContext } from "../auth/AuthContext"
+
+const actionsTemplate = (row, opts) => {
+    const { user } = useContext(AuthContext)
+    const id = opts.rowIndex
+    
+    return (
+        <div>
+            <Button icon='pi pi-pencil' />
+            { user?.role === 'admin' && <Button icon='pi pi-trash'  /> }
+        </div>
+    )
+}
 
 const Categories = () => {
+    const { user } = useContext(AuthContext)
+
     const [categories, setCategories] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
@@ -41,6 +56,9 @@ const Categories = () => {
                 {loading ? <ProgressSpinner/> :
                     <DataTable value={categories} emptyMessage='No hay categorías cargadas'>
                         <Column field="name" header='Nombre'/>
+                        { (user?.role === 'moderator' || user?.role === 'admin') &&
+                        <Column header='Acciones' body={actionsTemplate} style={{width: '15%'}} />
+                        }
                     </DataTable>
                 }
             </Card>
